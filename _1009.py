674d38f08a2a9d9512f3cb2aa132ad2e01d81548
Fábio Lima <lr.fabioml@gmail.com>
2016-03-21 00:19:34 -0400

    Adding 1009 to 1012
    1009 to 1012

--- _1009.py
+++ _1009.py
@@ -1,3 +1,11 @@
-a = input()
+# -*- coding: utf-8 -*-
 
-print (a)
+name = input()
+fixed_salary = input()
+sale_total = input()
+
+res = float(fixed_salary) + float(sale_total)*0.15
+
+str_res = "TOTAL = R$ %.2f" % (res)
+
+print (str_res)
